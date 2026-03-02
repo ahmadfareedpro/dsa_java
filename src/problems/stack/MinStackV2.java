cfe97a748295f7e7ff29c9291131830c2f54d4d8
ahmadfareedpro <ahmadfareed.pro@gmail.com>
2026-03-02 22:56:51 +0500

    added commentary on min stack sol v2 and completed next greater element problem.

--- src/problems/stack/MinStackV2.java
+++ src/problems/stack/MinStackV2.java
@@ -44,4 +44,19 @@ public class MinStackV2 {
             return -1;
         return (int)min;
     }
+
+    /*
+     * the approach:
+     * we store first value as min when stack was empty
+     * when new values comes in we store it as it is, because its grater than min, so min isn't changing
+     * if not we need to store it in min as well but to make sure we can get back to previus min
+     * we cannot replace min and stack top with this directly we need encoding of some kind
+     * so we, encode incoming value and store encoded value in stack, while real val in min as it was less than previous min
+     * encoding is important if we don't wanna use two stacks,
+     * because of encoding we can decode it from stack when popping element is less than min
+     * as it can never be less than min (because min should've had it any way) hence,
+     * we decode it and store the decoded value again in the min
+     * when popping: we check if it is less than min it means it was encoded
+     * we decode it and store the decoded value in min (reverse of push)
+     */
 }
--- src/problems/stack/NextGreater.java
+++ src/problems/stack/NextGreater.java
@@ -1,5 +1,7 @@
 package problems.stack;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class NextGreater {
@@ -12,6 +14,26 @@ public class NextGreater {
 
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        return null;
+        var res = new ArrayList<Integer>();
+        var stack = new ArrayDeque<Integer>();
+
+        for (var n : nums2) {
+            res.add(-1);
+        }
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                res.set(i, stack.peek());
+            }
+
+            stack.push(nums2[i]);
+        }
+
+        return res.stream().mapToInt(i -> i).toArray();
     }
 }
